fce19c25fb20493b83c9dde9dc8d385933c1a8cf
ravilella1234 <ravilella1234>
2019-10-18 12:45:55 +0530

    Browser commands

--- CarrierBridgeIT/src/com/icici/loans/carloans/BaseTest.java
+++ CarrierBridgeIT/src/com/icici/loans/carloans/BaseTest.java
@@ -1,5 +1,10 @@
 package com.icici.loans.carloans;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,14 +12,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BaseTest 
 {
 	public static WebDriver driver;
+	public static String projectPath=System.getProperty("user.dir");
+	public static Properties p;
+	
+	
+	public static void init() throws Exception
+	{
+		FileInputStream fis=new FileInputStream(projectPath+"//data.properties");
+		p=new Properties();
+		p.load(fis);
+	}
 	
 	public static void launch(String browser)
 	{
-		if(browser.equals("chrome")) 
+		if(p.getProperty(browser).equals("chrome")) 
 		{
 			driver=new ChromeDriver();
 		}
-		else if(browser.equals("firefox")) 
+		else if(p.getProperty(browser).equals("firefox")) 
 		{
 			driver=new FirefoxDriver();
 		}
@@ -22,7 +37,8 @@ public class BaseTest
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(url);
+		//driver.get(p.getProperty(url));
+		driver.navigate().to(p.getProperty(url));
 	}
 	
 	
--- CarrierBridgeIT/src/com/icici/loans/carloans/TC_001.java
+++ CarrierBridgeIT/src/com/icici/loans/carloans/TC_001.java
@@ -1,13 +1,44 @@
 package com.icici.loans.carloans;
 
+import org.openqa.selenium.By;
+
 public class TC_001 extends BaseTest
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
-		launch("chrome");
+		init();
 		
-		navigateUrl("https://www.amazon.in");
+		launch("chromebrowser");
+		
+		navigateUrl("hdfcurl");
+		
+		driver.manage().window().maximize();
+		
+		/*String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().refresh();*/
+		
+		driver.findElement(By.linkText("KNOW MORE")).click();
+		
+		//driver.close();
+		
+		driver.quit();
+		
 
 	}
 
